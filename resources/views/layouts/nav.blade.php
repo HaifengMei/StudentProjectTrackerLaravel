2e30b55642e367d5ba6f8917a877aaad9f9715a7
haifeng <haifengmei@hotmail.com>
2017-04-24 21:35:56 -0400

    removed my profile

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -33,9 +33,6 @@
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
